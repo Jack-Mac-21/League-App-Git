acf59e509275ad7cfc6bacd0f532f93a26674b61
Jack McCallie <65779661+Jack-Mac-21@users.noreply.github.com>
2021-12-18 20:32:12 -0600

    init + Player Class

---
+++ League App/League App/Player.swift
@@ -0,0 +1,23 @@
+//
+//  Player.swift
+//  League App
+//
+//  Created by JackMac on 12/18/21.
+//
+
+import Foundation
+import UIKit
+
+class Player{
+    var Name: String;
+    var Age: Int;
+    var SkillLevel: Double;
+    var Matches = [String: [[Int]]]();
+    
+    init(givenName: String, givenAge: Int, givenSkill: Double){
+        self.Name = givenName;
+        self.Age = givenAge;
+        self.SkillLevel = givenSkill;
+    }
+    
+}
---
+++ League App/League App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  League App
+//
+//  Created by JackMac on 12/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    var player1 = Player(givenName: "Jack", givenAge: 21, givenSkill: 4.0);
+
+
+}
+
