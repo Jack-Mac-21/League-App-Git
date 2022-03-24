07acde7e46f47f1c493ba1a6289f0b02369aaa82
Jack McCallie <65779661+Jack-Mac-21@users.noreply.github.com>
2022-03-24 11:17:52 -0500

    Back9 and Front9 Feature Added

--- League App/League App/Cells/PlayerCell.swift
+++ League App/League App/Cells/PlayerCell.swift
@@ -12,24 +12,49 @@ import UIKit
 //there score data displayed on the right
 class PlayerCell: UITableViewCell, DataDelegatePlayerListScreen{
     @IBOutlet weak var Name: UILabel!
+    
+    //Totals
     @IBOutlet weak var GrossLabel: UILabel!
     @IBOutlet weak var OverParLabel: UILabel!
+    
+    //Front 9
+    @IBOutlet weak var FrontGrossLabel: UILabel!
+    @IBOutlet weak var FrontOverParLabel: UILabel!
+    
+    //Back 9
+    @IBOutlet weak var BackGrossLabel: UILabel!
+    @IBOutlet weak var BackOverParLabel: UILabel!
+    
     
     var givenPlayer: Player?
     
     func setPlayer(player: Player){
         Name.text = player.Name
         player.calcScores()
+        
         GrossLabel.text = player.GrossScore?.description
-        OverParLabel.text = player.OverParScore?.description
+        OverParLabel.text =  player.OverParScore?.description
+        
+        FrontGrossLabel.text = player.Front9Gross?.description
+        FrontOverParLabel.text = player.Front9OverPar?.description
+        
+        BackGrossLabel.text = player.Back9Gross?.description
+        BackOverParLabel.text = player.Back9OverPar?.description
         
         
     }
     
     func updatePlayer(player: Player) {
-        givenPlayer = player
-        givenPlayer?.calcScores()
-        GrossLabel.text = givenPlayer?.GrossScore?.description
-        OverParLabel.text = givenPlayer?.OverParScore?.description
+        player.calcScores()
+        
+        GrossLabel.text = "Gross: " + (player.GrossScore?.description ?? "Error")
+        OverParLabel.text = "Over Par: " + (player.OverParScore?.description ?? "Error")
+        
+        FrontGrossLabel.text = player.Front9Gross?.description
+        FrontOverParLabel.text = player.Front9OverPar?.description
+        
+        BackGrossLabel.text = player.Back9Gross?.description
+        BackOverParLabel.text = player.Back9OverPar?.description
+        
     }
 }
--- League App/League App/ObjectClasses/Player.swift
+++ League App/League App/ObjectClasses/Player.swift
@@ -12,8 +12,15 @@ class Player: Codable{
     var Name: String;
     var Scores: [Int];
     var ParNums: [Int];
-    var OverParScore: Int?
-    var GrossScore: Int?
+    //Totals
+    var OverParScore: Int!
+    var GrossScore: Int!
+    //Back 9 scores
+    var Back9Gross: Int!
+    var Back9OverPar: Int!
+    //Front 9 scores
+    var Front9Gross: Int!
+    var Front9OverPar: Int!
     
     init(givenName: String){
         self.Name = givenName;
@@ -48,19 +55,46 @@ class Player: Codable{
     func calcScores(){
         var ScoreTotal = 0
         var ParTotal = 0
+        var Front9ScoreTotal = 0
+        var Back9ScoreTotal = 0
+        
+        var i = 0 as Int;
         for num in self.Scores{
             ScoreTotal += num
+            if i < 9{
+                Front9ScoreTotal += num
+            }
+            else{
+                Back9ScoreTotal += num
+            }
+            i += 1
         }
-        var i = 0 as Int;
+        var k = 0
+        i = 0
+        
+        var Front9ParTotal = 0
+        var Back9ParTotal = 0
         for num in self.ParNums{
-            if self.Scores[i] > 0 {
+            if self.Scores[k] > 0 {
                 ParTotal += num;
-                print(self.Scores[i])
+                if i < 9 {
+                    Front9ParTotal += num
+                }else{
+                    Back9ParTotal += num
+                }
             }
             i += 1;
+            k += 1;
         }
+        
         self.GrossScore = ScoreTotal
         self.OverParScore = ScoreTotal - ParTotal
+        
+        self.Front9Gross = Front9ScoreTotal
+        self.Front9OverPar = Front9ScoreTotal - Front9ParTotal
+        
+        self.Back9Gross = Back9ScoreTotal
+        self.Back9OverPar = Back9ScoreTotal - Back9ParTotal
         
     }
     
